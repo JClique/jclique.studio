d3e6b024f28cbc4fa463746263550698b32362a7
JClique <j.leadbetter@me.com>
2020-09-29 15:45:23 +0100

    Added Texture5

--- src/views/Texture.vue
+++ src/views/Texture.vue
@@ -2,6 +2,7 @@
   <div class="content">
     <img src="@/assets/texture/texture4.jpg" class="art">
     <img src="@/assets/texture/texture1.jpg" class="art">
+    <img src="@/assets/texture/texture5.jpg" class="art">
     <img src="@/assets/texture/texture2.jpg" class="art">
     <img src="@/assets/texture/texture3.jpg" class="art">
   </div>
